bcd597b9b5e7a5a835ef30c91fd475db7f26554c
Hiroki Kawai <syojikisyukatsu@yahoo.co.jp>
2019-08-07 00:37:50 +0900

    patch

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,13 +5,13 @@ app_path = File.expand_path('../../', __FILE__)
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/current"
+working_directory app_path
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen 3000
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
@@ -20,7 +20,7 @@ stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
-timeout 180
+timeout 60
 
 #以下は応用的な設定なので説明は割愛
 
